6e9605c10b520df7e676deac202ce16f05727abb
Ivan <villamar.ivan@yahoo.com>
2018-03-17 21:17:35 -0700

    adds current working code

--- TrackMasterBot/routes/index.js
+++ TrackMasterBot/routes/index.js
@@ -16,13 +16,14 @@ bot.on([/^\/track (.+)$/, '/track productName'], (msg, props) => {
   const productName = props.match[1];
   const text = productName + ' is currently at San Francisco, CA 94132!';
   return bot.sendMessage(msg.from.id, text) + bot.sendLocation(msg.from.id, [37.7265802, -122.4907688]); 
+  //return bot.sendMessage(msg.from.id, text) + bot.sendPhoto(msg.from.id, 'https://www.google.com/maps/dir/singapore/san+francisco/@15.461773,131.8116818,3z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x31da11238a8b9375:0x887869cf52abf5c4!2m2!1d103.819836!2d1.352083!1m5!1m1!1s0x80859a6d00690021:0x4a501367f076adff!2m2!1d-122.4194155!2d37.7749295');
 });
 
 /* /temperature command tells user current temperature of the product */
 bot.on([/^\/temperature (.+)$/, '/temperature productName'], (msg, props) => {
   const productName = props.match[1];
   const text = productName + ' is at 90 °Fahrenheit!';
-  return bot.sendMessage(msg.from.id, text);
+  return bot.sendMessage(msg.from.id, text) + bot.sendPhoto(msg.from.id, 'https://www.amcharts.com/demos/date-based-line-chart/?theme=dark');
 });
 
 /* /time command tells user ETA of the product */
